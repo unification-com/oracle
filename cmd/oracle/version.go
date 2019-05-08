adec80389a97078fb5109b80750aab3a0edb3704
Codegnosis <paul@codegnosis.co.uk>
2019-05-08 11:54:08 +0100

    cmd.oracle - add version handler

---
+++ cmd/oracle/version.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+const (
+	VersionMajor = 0        // Major version component of the current release
+	VersionMinor = 1        // Minor version component of the current release
+	VersionPatch = 0        // Patch version component of the current release
+	VersionMeta  = "dev" // Version metadata to append to the version string
+)
+
+// Version holds the textual version string.
+var Version = func() string {
+	v := fmt.Sprintf("%d.%d.%d", VersionMajor, VersionMinor, VersionPatch)
+	if VersionMeta != "" {
+		v += "-" + VersionMeta
+	}
+	return v
+}()
